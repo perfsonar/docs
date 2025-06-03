34907ffaa6542d695a66b5c2fb779ea6695880c4
Lætitia Delvaux <laetitiaadelvaux@gmail.com>
2025-06-03 12:18:50 +0200

    deploy

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.1.3',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '5.1.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
